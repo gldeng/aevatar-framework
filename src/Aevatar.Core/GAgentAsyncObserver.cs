ebc8d33d5fd606997d13116d1872b39998ac786b
gldeng <realgldeng@gmail.com>
2025-04-14 14:32:14 +0800

    feat(obs): add tracing for event handling in grains

--- src/Aevatar.Core/GAgentAsyncObserver.cs
+++ src/Aevatar.Core/GAgentAsyncObserver.cs
@@ -6,25 +6,40 @@ namespace Aevatar.Core;
 public class GAgentAsyncObserver : IAsyncObserver<EventWrapperBase>
 {
     private readonly List<EventWrapperBaseAsyncObserver> _observers;
+    private readonly string _grainId;
 
-    public GAgentAsyncObserver(List<EventWrapperBaseAsyncObserver> observers)
+    public GAgentAsyncObserver(List<EventWrapperBaseAsyncObserver> observers, string grainId)
     {
         _observers = observers;
+        _grainId = grainId;
     }
     
     public async Task OnNextAsync(EventWrapperBase item, StreamSequenceToken? token = null)
     {
         var eventType = (EventBase)item.GetType().GetProperty(nameof(EventWrapper<EventBase>.Event))?.GetValue(item)!;
-        // TODO: Maybe use RuleEngine to optimize this.
-        var matchedObservers = _observers.Where(observer =>
-            observer.ParameterTypeName == eventType.GetType().Name ||
-            observer.ParameterTypeName == nameof(EventWrapperBase) ||
-            observer.MethodName == AevatarGAgentConstants.ForwardEventMethodName ||
-            observer.MethodName == AevatarGAgentConstants.ConfigDefaultMethodName).ToList();
-        foreach (var observer in matchedObservers)
+        
+        using var scope = OpenTelemetryScope.Start(_grainId, eventType, token);
+
+        try
         {
-            await observer.OnNextAsync(item);
+            // TODO: Maybe use RuleEngine to optimize this.
+            var matchedObservers = _observers.Where(observer =>
+                observer.ParameterTypeName == eventType.GetType().Name ||
+                observer.ParameterTypeName == nameof(EventWrapperBase) ||
+                observer.MethodName == AevatarGAgentConstants.ForwardEventMethodName ||
+                observer.MethodName == AevatarGAgentConstants.ConfigDefaultMethodName).ToList();
+            foreach (var observer in matchedObservers)
+            {
+                // TODO: add tracing for individual observer
+                await observer.OnNextAsync(item);
+            }            
         }
+        catch (Exception ex)
+        {
+            scope.RecordException(ex);
+            throw;
+        }
+
     }
 
     public async Task OnCompletedAsync()
