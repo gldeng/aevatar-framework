d11136a16f35211603c7bfb0b7d1781d0b4640a7
gldeng <realgldeng@gmail.com>
2025-04-16 16:04:30 +0800

    feat(obs):  add tracing

--- src/Aevatar.Core/GAgentBase.cs
+++ src/Aevatar.Core/GAgentBase.cs
@@ -226,7 +226,7 @@ public abstract partial class
     {
         var streamOfThisGAgent = GetEventBaseStream(this.GetGrainId().ToString());
         var handles = await streamOfThisGAgent.GetAllSubscriptionHandles();
-        var asyncObserver = new GAgentAsyncObserver(_observers);
+        var asyncObserver = new GAgentAsyncObserver(_observers, this.GetGrainId().ToString());
         if (handles.Count > 0)
         {
             foreach (var handle in handles)
